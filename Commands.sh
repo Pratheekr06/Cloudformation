154a94af751e10fd845500a4e912628afefa7924
Pratheek R <pratheek.r@oracle.com>
2023-10-08 16:26:06 +0530

    simple ec2 instance

--- Commands.sh
+++ Commands.sh
@@ -1,3 +1,5 @@
 VPC:
   aws cloudformation validate-template --template-body file://VPC.yaml
   aws cloudformation create-stack --stack-name vpc-stack --template-body file://VPC.yaml --region ap-south-1 
+EC2:
+  aws cloudformation create-stack --stack-name ec2-stack --template-body file://SimpleEC2Instance.yaml --parameters ParameterKey=KeyName,ParameterValue=pratheekEC2 --region ap-south-1
