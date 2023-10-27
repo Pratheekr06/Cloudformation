a0506f25a59d0979d213a159991307ae9399d087
Pratheek R <pratheek.r@oracle.com>
2023-10-27 13:06:19 +0530

    s3 bucket with lambda notification

--- Commands.sh
+++ Commands.sh
@@ -3,3 +3,5 @@ VPC:
   aws cloudformation create-stack --stack-name vpc-stack --template-body file://VPC.yaml --region ap-south-1 
 EC2:
   aws cloudformation create-stack --stack-name ec2-stack --template-body file://SimpleEC2Instance.yaml --parameters ParameterKey=KeyName,ParameterValue=pratheekEC2 --region ap-south-1
+Lambda:
+  aws cloudformation update-stack --stack-name s3-lambda-stack --template-body file://Lambda.yaml --parameters ParameterKey=S3BucketName,ParameterValue=BUCKET_NAME ParameterKey=LambdaCodeBucketName,ParameterValue=LAMBDA_CODE_BUCKET_NAME --capabilities CAPABILITY_NAMED_IAM --profile default --region ap-south-1
